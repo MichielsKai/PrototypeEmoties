4e6eada21a1b7858b479e534c09763e207b2920d
MichielsKai <Kai.Michiels.Work@gmail.com>
2025-05-09 00:29:52 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function getParams() {
+    var idx = document.URL.indexOf("?");
+    var params = new Array();
+    if (idx != -1) {
+        var pairs = document.URL.substring(idx + 1, document.URL.length).split(
+            "&"
+        );
+        for (var i = 0; i < pairs.length; i++) {
+            nameVal = pairs[i].split("=");
+            params[nameVal[0]] = nameVal[1];
+        }
+    }
+    return params;
+}
+params = getParams();
+console.log(params.blij);
+console.log(params.boos);
+console.log(params.verdrietig);
+console.log(params.bang);
